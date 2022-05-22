945452e48949113dfca4735d21d901b66dd4921f
Ricardo Branco <ricardo.vezetiv@mail.ru>
2022-05-22 20:12:31 -0300

    Adicionado uma 'navbar', criado os campos 'task' e 'completed', estilizada a página 'home' para visualizar as tarefas cadastradas, habilitado o formulário do Django para o cadastro de novas tarefas e criado a rotina de exclusão para as tarefas listadas.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('delete/<int:todo_id>', views.delete, name='delete'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'todo_app/home.html')
+    if request.method == 'POST':
+        form = TodoForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            todos = Todo.objects.all()
+            messages.success(request, ('Task has been added!'))
+            return render(request, 'todo_app/home.html', {'todos': todos})
+    else:
+        todos = Todo.objects.all()
+        return render(request, 'todo_app/home.html', {'todos': todos})
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    messages.success(request, ('Task has been Deleted!'))
+    return redirect('home')
