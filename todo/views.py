06507b7e34789a96497b570df75e57d0aae61edc
Ricardo Branco <ricardo.vezetiv@mail.ru>
2022-05-26 10:06:38 -0300

    Alterado no nome da aplicação para 'todo' e refatoração do código com base no nome da nova aplicação.

--- todo/views.py
+++ todo/views.py
@@ -11,17 +11,17 @@ def home(request):
         if form.is_valid():
             form.save()
             todos = Todo.objects.all()
-            messages.success(request, ("Task has been added!"))
-            return render(request, "todo_app/home.html", {"todos": todos})
+            messages.success(request, "Task has been added!")
+            return render(request, "todo/home.html", {"todos": todos})
     else:
         todos = Todo.objects.all()
-        return render(request, "todo_app/home.html", {"todos": todos})
+        return render(request, "todo/home.html", {"todos": todos})
 
 
 def delete(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.delete()
-    messages.success(request, ("Task has been Deleted!"))
+    messages.success(request, "Task has been Deleted!")
     return redirect("home")
 
 
@@ -46,8 +46,8 @@ def edit(request, todo_id):
 
         if form.is_valid():
             form.save()
-            messages.success(request, ("Task has been edited!"))
+            messages.success(request, "Task has been edited!")
             return redirect("home")
     else:
         todo = Todo.objects.get(id=todo_id)
-        return render(request, "todo_app/edit.html", {"todo": todo})
+        return render(request, "todo/edit.html", {"todo": todo})
